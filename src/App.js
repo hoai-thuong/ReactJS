1fcb4675a3dfb3d63657bb5df632c5741606dac4
hoaithuong <thuongcth03@gmail.com>
2023-08-10 10:14:59 +0700

    props in functional component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello'
 import Message  from './components/Message';
+import Profile from './components/Profile';
 function App() {
   return (
     <div className="App">
-      <Hello/>
-      <Message></Message>
+      {/* <Hello/>
+      <Message></Message> */}
+      <Profile name = "Hoai" lastname = "Thuong">
+        <h1>is the most beautifull</h1>
+      </Profile>
+
       
     </div>
   );
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,5 +2,9 @@
 //     return <h1>Hoai Thuong xinh dep</h1>
 // }
 
-const Hello = ()=> <h1>Hello</h1>
-export default Hello 
+const displayMessage = ()=> <h1>Hello</h1>
+function Hello() {
+    return <h1>This is message : {displayMessage}</h1>
+}
+
+export default Hello  
